e12e32dd36386df9e27cb5c5ff8e76ca60cd5bc0
bobnearents <bobnearents@yahoo.com>
2019-01-30 09:51:05 -0800

    added api.js

---
+++ scripts/api.js
@@ -0,0 +1,35 @@
+'use strict'
+const api = function() {
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com';
+  const key = 'bob-owen';
+
+  const apiCall = function (...args) {
+    return fetch(...args)
+    .then(res => res.json())
+  }
+
+  const getBookmark = function () {
+    return apiCall(`${BASE_URL}/${key}/bookmarks`)
+  }
+
+  const createItem = function(item) {
+    return apiCall(`${BASE_URL}/${key}/bookmarks`, {
+      method: 'POST',
+      headers: {
+      'Content-Type':'application/json'
+      },
+      body: item
+    })
+  }
+
+  const deleteItem = function () {
+
+  }
+  
+
+
+  return {
+    getBookmark,
+    createItem
+  }
+}()
